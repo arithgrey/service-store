12a827b965cdcfb5fd6e350949f7c9e799ab4170
arithgrey <arithgrey@gmail.com>
2024-08-13 01:34:21 -0600

    [FEAT] Stock service views

---
+++ src/components/Products/ProductConfig.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <TransitionRoot as="template" :show="open">
+      <Dialog as="div" class="relative z-50" @close="open = false">
+        <TransitionChild
+          as="template"
+          enter="ease-in-out duration-500"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in-out duration-500"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div
+            class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          />
+        </TransitionChild>
+
+        <div class="fixed inset-0 overflow-hidden">
+          <div class="absolute inset-0 overflow-hidden">
+            <div
+              class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10"
+            >
+              <TransitionChild
+                as="template"
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enter-from="translate-x-full"
+                enter-to="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leave-from="translate-x-0"
+                leave-to="translate-x-full"
+              >
+                <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                  <div
+                    class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl"
+                  >
+                    <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                      <div class="flex items-start justify-between">
+                        <DialogTitle class="text-lg font-medium text-gray-900"
+                          >Configuración</DialogTitle
+                        >
+                      
+                        <div class="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            class="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
+                            @click="open = false"
+                          >
+                            <span class="absolute -inset-0.5" />
+                            <span class="sr-only">Close panel</span>
+                            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                          </button>
+                        </div>
+                        
+                      </div>
+                      <p class="text-sm text-gray-500">ID del producto: {{ product.id }}</p>
+                      <p class="text-sm text-gray-500">Cantidad disponible: {{ quantity }}</p>
+
+                    </div>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+  </div>
+</template>
+
+<script>
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  XMarkIcon,
+} from "@heroicons/vue/24/outline";
+
+import {
+  Dialog,   
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+
+export default {
+  components: {
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    TransitionChild,
+    TransitionRoot,
+    Bars3Icon,
+    MagnifyingGlassIcon,
+    XMarkIcon,
+  },
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      open: false,
+      quantity: null,
+    };
+  },
+  watch: {
+    'product.id': {
+      immediate: true,
+      handler(newVal) {
+        this.fetchStock();
+      },
+    },
+  },
+  methods: {
+    async fetchStock() {
+      try {
+        
+        const params = { product_id: this.product.id };
+        const response = await this.$axios.get(`/stock/movements/stock-movements/quantity/`, {params});    
+        this.quantity = response.data.quantity;  
+
+      } catch (error) {
+        console.error("Error stock products:", error);
+      }
+    },
+    openCart() {
+      this.open = true;
+    },
+    closeCart() {
+      this.open = false;
+    },
+  }
+};
+</script>

a41985973fac7fd851b7702899be57631c5310d2
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2025-03-18 19:47:12 -0600

    [chore]

---
+++ src/config/urls.js
@@ -0,0 +1,6 @@
+export const API_URLS = {
+    ASSISTANT: {
+      SEND_MESSAGE: '/assistant/assistant/send-message/'
+                    
+    }
+  }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from '@/router';
 import axiosInstance from "@/axiosInstance";
 import store from '@/store';
 import EcommerceHelper from '@/helpers/EcommerceHelper';
+import Assistant from '@/components/Bot/Assistant.vue'
 
 const app = createApp(App);
 
@@ -13,4 +14,5 @@ app.use(store);
 app.mixin(EcommerceHelper);
 
 app.config.globalProperties.$axios = axiosInstance;
+app.component('Assistant', Assistant)
 app.mount('#app');
